682c805c1e92923e0bce5b3a966ff1545b954ab3
karl <5895483+karlivory@users.noreply.github.com>
2023-10-08 16:07:44 +0300

    nvim autocmd: fix markdown indenting

--- dotfiles/.config/nvim/lua/user/autocmds.lua
+++ dotfiles/.config/nvim/lua/user/autocmds.lua
@@ -1,13 +1,23 @@
+local function setindent(indent)
+  vim.bo.expandtab = (indent > 0)
+  indent = math.abs(indent)
+  vim.bo.tabstop = indent
+  vim.bo.softtabstop = indent
+  vim.bo.shiftwidth = indent
+  require("guess-indent").set_from_buffer()
+end
+
+local function indenter(indent)
+  vim.loop.new_timer():start(200, 0, vim.schedule_wrap(function() setindent(indent) end))
+end
+
 -- some kind of bug w/ markdown & GuessIndent not working properly (TODO: github issue?)
 vim.api.nvim_create_autocmd(
-  { "BufRead", },
+  { "BufEnter", },
   {
     pattern = "*.md",
     callback = function()
-      local function setindent()
-        require("guess-indent").set_from_buffer()
-      end
-      vim.loop.new_timer():start(200, 0, vim.schedule_wrap(setindent))
+      indenter(2)
     end
   }
 )
