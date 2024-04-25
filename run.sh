63f4c88b7121f46bcf4944d99b5afc956e7137f7
karl <5895483+karlivory@users.noreply.github.com>
2024-04-25 14:00:57 +0300

    fixes

--- run.sh
+++ run.sh
@@ -4,22 +4,30 @@ ARGS="$@"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 VENV_PATH="$DIR/venv"
 
-setup_venv() {
-    command -v /usr/bin/virtualenv >/dev/null 2>&1 || fail "virtualenv is required but it's not installed."
+prerequisites() {
+    if ! command -v /usr/bin/virtualenv >/dev/null 2>&1; then
+        sudo apt install virtualenv
+    fi
+    if ! command -v pip >/dev/null 2>&1; then
+        sudo apt install python3-pip
+    fi
+    if ! command -v python >/dev/null 2>&1; then
+        sudo apt install python-is-python3
+    fi
+}
 
+setup_venv() {
     # Create a virtual environment if it doesn't exist
     if [ ! -d "$VENV_PATH" ]; then
         echo "Creating a virtual environment..."
         /usr/bin/virtualenv "$VENV_PATH"
     fi
-
     source "$VENV_PATH/bin/activate"
-
     echo "Installing pip requirements..."
     pip install -r "$DIR/requirements.txt"
-
 }
 
+prerequisites
 setup_venv
 
 ansible-galaxy collection install -r roles/requirements.yml
