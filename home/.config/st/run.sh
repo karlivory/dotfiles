03aac907756667b5ff3a841c1faf7912cfa2175d
karl <5895483+karlivory@users.noreply.github.com>
2024-04-30 18:18:30 +0300

    fixes

--- home/.config/st/run.sh
+++ home/.config/st/run.sh
@@ -1,9 +1,28 @@
 # !/usr/bin/env bash
 
-cd st-flexipatch
+set -e
 
-sudo rm patches.h config.h
-cp ../patches.h .
-git apply ../st.patch
+if [[ -z "${FLEXIPATCH}" ]]; then
+  echo "FLEXIPATCH not set"
+  exit 1
+fi
+
+cd "$FLEXIPATCH-flexipatch"
+
+git apply "../$FLEXIPATCH.patch"
+
+if [ ! -f config.h ] || ! cmp -s config.def.h config.h; then
+  sudo cp config.def.h config.h
+fi
+if [ ! -f patches.h ] || ! cmp -s ../patches.h patches.h; then
+  sudo cp ../patches.h patches.h
+fi
+
+sudo cp "$FLEXIPATCH" "../$FLEXIPATCH.bak"
 sudo make install
-git apply ../st.patch --reverse
+
+if [ "$(md5sum "../$FLEXIPATCH.bak" | cut -d ' ' -f1)" != "$(md5sum "$FLEXIPATCH" | cut -d ' ' -f1)" ]; then
+  echo "TASK_CHANGED"
+fi
+
+git apply "../$FLEXIPATCH.patch" --reverse
