f17ea31a169bc2fe403e197b7a5123b68679c7c2
karl <5895483+karlivory@users.noreply.github.com>
2025-12-06 13:53:28 +0200

    update packages

--- home/.config/homebrew-formulas/jetbrains-toolbox.rb
+++ home/.config/homebrew-formulas/jetbrains-toolbox.rb
@@ -1,14 +1,15 @@
+# source: https://www.jetbrains.com/toolbox-app/download/download-thanks.html?platform=linux
 class JetbrainsToolbox < Formula
   desc "Jetbrains Toolbox"
   homepage "https://www.jetbrains.com/toolbox/app/"
-  version "2.5.1.34629"
-  url "https://download-cdn.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.tar.gz"
+  version "3.1.2.64642"
+  url "https://download.jetbrains.com/toolbox/jetbrains-toolbox-#{version}.tar.gz"
 
-  sha256 "a082bb0a0f3d51240ad7cb64c9fe6492cfd17dc22c7cc17695a66f8beaaef6d6"
+  # sha256 "a082bb0a0f3d51240ad7cb64c9fe6492cfd17dc22c7cc17695a66f8beaaef6d6"
 
   def install
     libexec.install Dir["*"]
-    bin.install_symlink("#{libexec}/jetbrains-toolbox" => "jetbrains-toolbox")
+    bin.install_symlink("#{libexec}/bin/jetbrains-toolbox" => "jetbrains-toolbox")
   end
 
   test do
